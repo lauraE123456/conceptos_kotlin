d6dd167e3abfdc66dacca90d15a5fdef32a21327
ASUS <jeissuvan@gmail.com>
2024-07-15 21:59:31 -0500

    primer commit

---
+++ conceptos_kotlin.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("CONCEPTOS KOTLIN")
+    println("Variables: En Kotlin, una variable se declara usando la palabra clave var para mutables o 
+    val para inmutables. Las variables mutables (var) pueden cambiar su valor después de la inicialización, 
+    mientras que las variables inmutables (val) no pueden cambiar su valor una vez que se les asigna.")
+    
+    println("Constantes: En Kotlin, una constante se declara usando la palabra clave val. Una vez que se asigna un valor a una 
+    constante, no se puede cambiar. Es similar a una variable inmutable.")
+
+    println("Opcionales: En Kotlin, el concepto de "opcionales" no es directamente aplicable como en otros lenguajes que usan el término para referirse a tipos que pueden contener un valor nulo o no tener valor (como en Swift o Kotlin con tipos anulables). En Kotlin, los tipos pueden 
+    ser anulables mediante el uso del operador ?, lo cual indica que una variable puede contener null.")
+
+    println("Nulos: En Kotlin, el concepto de null es manejado de manera segura. Los tipos en Kotlin no pueden tener un valor null a menos que se especifique explícitamente añadiendo ? después del tipo (por ejemplo, String? en lugar de String). Esto ayuda a 
+    evitar los errores de referencia nula (NullPointerException) comunes en otros lenguajes.")
+
+    println("README.md: Este archivo es comúnmente utilizado en repositorios de código para proporcionar información sobre el proyecto, instrucciones de instalación, documentación 
+    sobre cómo usar el código, entre otros detalles útiles para los desarrolladores y usuarios del proyecto.)
+}
